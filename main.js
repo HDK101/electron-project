6bfabd569beaed43c8795cf6dfdc4216be2a71e5
Lucas Hideki <lucashideki87@gmail.com>
2020-01-06 19:51:09 -0300

    Main files

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const { app, BrowserWindow } = require("electron");
 
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile("index.html");
+
+  win.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
