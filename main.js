b450674165ee81ec0f81b858add4c88be7c5f76e
Lucas Hideki <lucashideki87@gmail.com>
2020-01-06 20:44:54 -0300

    Frame set to false

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function createWindow() {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
-    // frame: false,
+    frame: false,
     webPreferences: {
       nodeIntegration: true
     }
