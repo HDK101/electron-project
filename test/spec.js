182c9fe9856fdb674ec94a3a3df7986e89dd264e
Lucas Hideki <lucashideki87@gmail.com>
2020-01-07 14:08:39 -0300

    Written tests with Spectron

---
+++ test/spec.js
@@ -0,0 +1,28 @@
+const { Application } = require("spectron");
+const assert = require("assert");
+const electronPath = require("electron");
+const path = require("path");
+
+describe("Application launch", () => {
+  this.timeOut(10000);
+
+  beforeEach(() => {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname), ".."]
+    });
+    return this.app.start();
+  });
+
+  afterEach(() => {
+      if(this.app && this.app.isRunning()) {
+          return this.app.stop();
+      }
+  });
+
+  it("Show initial window", () => {
+      return this.app.client.getWindowCount().then((count) => {
+        assert.equal(count, 1);
+      });
+  });
+});
