dc33b3b17c2075a28366d459a3cf874d058ab66a
Lucas Hideki <lucashideki87@gmail.com>
2020-01-07 14:25:44 -0300

    Written simple test for Electron

--- test/spec.js
+++ test/spec.js
@@ -3,25 +3,25 @@ const assert = require("assert");
 const electronPath = require("electron");
 const path = require("path");
 
-describe("Application launch", () => {
-
-  beforeEach(() => {
+describe("Application launch", function() {
+  beforeEach(function() {
+    this.timeout(10000);
     this.app = new Application({
-      path: electronPath,
-      args: [path.join(__dirname), ".."]
+      path: electronPath
+      //   args: [path.join(__dirname), "../"]
     });
     return this.app.start();
   });
 
-  afterEach(() => {
-      if(this.app && this.app.isRunning()) {
-          return this.app.stop();
-      }
+  afterEach(function() {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
   });
 
-  it("Show initial window", () => {
-      return this.app.client.getWindowCount().then((count) => {
-        assert.equal(count, 1);
-      });
+  it("Show initial window", function() {
+    return this.app.client.getWindowCount().then(count => {
+      assert.equal(count, 1);
+    });
   });
 });
